f8a1cf0a1dd9a8fd8b53c46d7faf4f3fc031b51e
ng <noam.gallmann@gmail.com>
2025-12-25 01:21:03 +0100

    add backlight, unify

--- config.h
+++ config.h
@@ -2,13 +2,13 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER "  "
+#define DELIMITER " | "
 
 // Maximum number of Unicode characters that a block can output.
 #define MAX_BLOCK_OUTPUT_LENGTH 45
 
 // Control whether blocks are clickable.
-#define CLICKABLE_BLOCKS 1
+#define CLICKABLE_BLOCKS 0
 
 // Control whether a leading delimiter should be prepended to the status.
 #define LEADING_DELIMITER 0
@@ -20,6 +20,7 @@
 
 // X(ICON, CMD, update_interval, update_signal
 #define BLOCKS(X) \
-    X("", "$CONFIGROOT/../DEPS/scripts/shell/volume --get ", 0 ,5)
+    X("", "echo volume: $($CONFIGROOT/../DEPS/scripts/shell/volume --get)%", 0 ,5) \
+    X("", "echo backlilght: $($CONFIGROOT/../DEPS/scripts/shell/backlight --get)%", 0 ,6)
 
 #endif  // CONFIG_H
